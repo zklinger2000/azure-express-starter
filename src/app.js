18b8160bead4692652271bee60d2c75695094fc4
Zack Klinger <zack@zklinger.com>
2018-03-18 02:21:46 -0700

    add cors module to test npm installing during deployment

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import bodyParser from 'body-parser';
 import routes from './routes/routes';
 import connectDB from './database/connectDB';
 // import config from '../config';
-// import cors from 'cors';
+import cors from 'cors';
 
 // TODO: add tests
 
