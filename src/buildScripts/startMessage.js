ec6b55fe0e15167e9e609ae27977ee8c177fe118
Zachary Klinger <zklinger@zmac.local>
2018-03-19 22:43:14 -0700

    dev(log): Add processor and node version to startup message

--- src/buildScripts/startMessage.js
+++ src/buildScripts/startMessage.js
@@ -2,6 +2,8 @@
 import chalk from 'chalk';
 import config from '../../config';
 
+console.log(chalk.blueBright('arch:'), process.arch, chalk.blueBright('node:'), process.version);
+
 console.log(chalk.green('Starting app in'),
   chalk.blue(config.nodeEnv),
   chalk.green('mode...'));
